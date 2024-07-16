963a096fd2d5527b13d2acf83cd9168903f677ec
Shirley Tatiana García <shirleygarcia@wposs.con>
2024-07-16 16:55:23 -0500

    update project with third project

--- 03-mouse-follower/src/App.jsx
+++ 03-mouse-follower/src/App.jsx
@@ -1,12 +1,53 @@
 import './App.css'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 function App() {
-  <>
+  const [enabled, setEnabled] = useState(false);
+  const [position, setPosition] = useState({x:0, y:0});
+
+  useEffect(()=>{
+ 
+    console.log('cuando se actualice el estado');
+    const handleMove = (event)=>{
+      const {clientX, clientY} = event;
+      setPosition({x: clientX, y: clientY})
+      console.log('handleMove', {clientX, clientY});
+    }
+
+    if(enabled){
+      console.log('entra al if');
+      window.addEventListener('pointermove', handleMove)
+    } 
+
+    return () => {
+      console.log('cleanup');
+      window.removeEventListener('pointermove', handleMove)
+      setPosition({x: 0, y: 0})
+    }
+
+   
+  }, [enabled])
   
-  </>
   return (
-   <h1>Hola</h1>
+    <main>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: '#09f',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40, 
+        transform: `translate(${position.x}px,${position.y}px)`
+      }}>
+    
+
+      </div>
+      <button onClick={()=>{setEnabled(!enabled)}}>{enabled ? 'Desactivar': 'Activar'} seguir puntero</button>
+    </main>
+  
   )
 }
 
